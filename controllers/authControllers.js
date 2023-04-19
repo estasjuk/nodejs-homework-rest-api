1410e67976414929afa409f13f0a9fecc4676110
Olena Stasiuk <estasjuk@gmail.com>
2023-04-20 00:33:53 +0300

    add mailing

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
+const { nanoid } = require('nanoid');
 const path = require('path');
 const fs = require('fs/promises');
 const resizeAvatar = require('../utils/resizeAvatar');
 const HttpError = require('../helpers/HttpError');
+const sendEmail = require('../helpers/sendEmail');
 
 const { User } = require('../models/users');
 const { controllerWrapper } = require('../utils/contollerWrapper');
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 
 const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
@@ -23,7 +25,18 @@ const registerUser = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
-    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
+    const verificationToken = nanoid();
+
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+    
+     const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`
+    };
+
+    await sendEmail(verifyEmail);
+
     res.status(201).json({
         status: 'success',
         code: 201,
@@ -34,11 +47,56 @@ const registerUser = async (req, res) => {
     },);
 };
 
+const verifyEmail = async(req, res)=> {
+    const {verificationToken} = req.params;
+    const user = await User.findOne({verificationToken});
+    if(!user){
+        throw new HttpError(404, "User not found")
+    }
+    await User.findByIdAndUpdate(user._id, {verify: true, verificationToken: ""});
+
+    res.status(200).json({
+        status: 'success',
+        code: 200,
+        message: "Verification is successful"
+    })
+}
+
+const resendVerifyEmail = async (req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new HttpError(401, "Email not found");
+    }
+    if (user.verify) {
+        throw new HttpError(400, "Verification has already been passed");
+    }
+
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${user.verificationCode}">Click verify email</a>`
+    };
+
+    await sendEmail(verifyEmail);
+
+    res.status(200).json({
+        status: 'success',
+        code: 200,
+        message: "Verification email sent"
+    })
+};
+
 const loginUser = async (req, res) => {
     const { email, password} = req.body;
     const user = await User.findOne({ email });
     if (!user) { 
         throw new HttpError(401, "Email or password is wrong");
+    }
+
+    if (!user.verify) { 
+        throw new HttpError(400, "Email is not verified");
     }
 
     const passwordCompare = await bcrypt.compare(password, user.password);
@@ -120,6 +178,8 @@ const updateAvatar = async (req, res) => {
 
 module.exports = {
     registerUser: controllerWrapper(registerUser),
+    verifyEmail: controllerWrapper(verifyEmail),
+    resendVerifyEmail: controllerWrapper(resendVerifyEmail),
     loginUser: controllerWrapper(loginUser),
     getCurrent: controllerWrapper(getCurrent),
     logoutUser: controllerWrapper(logoutUser),
--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -4,12 +4,14 @@ const validateBody = require('../../utils/validateBody');
 const upload = require('../../middlewares/upload');
 const authenticate = require('../../middlewares/authenticate');
 
-const { registerUser, loginUser, getCurrent, logoutUser, updateUserSubscription, updateAvatar } = require('../../controllers/authControllers');
-const { registerSchema, loginSchema, updateSubscriptionSchema } = require('../../schemas/usersSchema');
+const { registerUser, verifyEmail, resendVerifyEmail, loginUser, getCurrent, logoutUser, updateUserSubscription, updateAvatar } = require('../../controllers/authControllers');
+const { registerSchema, emailSchema, loginSchema, updateSubscriptionSchema } = require('../../schemas/usersSchema');
 
 const router = express.Router();
 
 router.post('/register', validateBody(registerSchema), registerUser);
+router.get('/verify/:verificationToken', verifyEmail);
+router.post("/verify", validateBody(emailSchema), resendVerifyEmail);
 router.post('/login', validateBody(loginSchema), loginUser);
 router.get('/current', authenticate, getCurrent);
 router.post('/logout', authenticate, logoutUser);
